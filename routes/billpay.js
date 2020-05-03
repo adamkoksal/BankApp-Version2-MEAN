e03c2d35eec60fca0394ac224547482d3b2cda83
adamkoksal <a.koksal02@gmail.com>
2020-05-02 23:34:40 -0500

    BillPay Implemented

--- routes/billpay.js
+++ routes/billpay.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const BillPay = require("../model/BillPay");
 
+router.get("/", async (req, res) => {
+  await BillPay.find()
+    .then((data) => res.send(data))
+    .catch((err) => res.send(err));
+});
+
 router.post("/", async (req, res) => {
   await new BillPay(req.body)
     .save()
