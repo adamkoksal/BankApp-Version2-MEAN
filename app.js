3f0345bc8d5a7aae3b817a1642758cd04b30b6c7
adamkoksal <a.koksal02@gmail.com>
2020-05-04 22:43:28 -0500

    Commit

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 require("dotenv").config();
 require("./startup/logging")();
 require("./startup/db")();
 app.use(express.json());
 require("./startup/routes")(app);
 require("./startup/prod")(app);
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
